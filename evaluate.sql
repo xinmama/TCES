4b4cf79bc51f285aacdc6b1e49c518c8f8fe7870
xinmama <54787038+xinmama@users.noreply.github.com>
2019-09-10 09:21:26 +0800

    Add files via upload

---
+++ evaluate.sql
@@ -0,0 +1,225 @@
+﻿# Host: localhost  (Version 5.7.22)
+# Date: 2019-09-06 09:32:48
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "admin"
+#
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "admin"
+#
+
+
+#
+# Structure for table "classes"
+#
+
+DROP TABLE IF EXISTS `classes`;
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `classes_no` varchar(20) NOT NULL DEFAULT '',
+  `dep_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "classes"
+#
+
+
+#
+# Structure for table "course"
+#
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(20) NOT NULL DEFAULT '',
+  `dep_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "course"
+#
+
+
+#
+# Structure for table "department"
+#
+
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dep_name` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "department"
+#
+
+
+#
+# Structure for table "option"
+#
+
+DROP TABLE IF EXISTS `option`;
+CREATE TABLE `option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title_id` int(11) NOT NULL DEFAULT '0',
+  `option_con` varchar(20) NOT NULL DEFAULT '',
+  `option_score` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "option"
+#
+
+
+#
+# Structure for table "st_score"
+#
+
+DROP TABLE IF EXISTS `st_score`;
+CREATE TABLE `st_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL DEFAULT '0',
+  `classes_id` int(11) NOT NULL DEFAULT '0',
+  `teacher_id` int(11) NOT NULL DEFAULT '0',
+  `st_score` float(5,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "st_score"
+#
+
+
+#
+# Structure for table "student"
+#
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_no` varchar(20) NOT NULL DEFAULT '',
+  `student_name` varchar(20) NOT NULL DEFAULT '',
+  `student_pwd` varchar(20) NOT NULL DEFAULT '',
+  `classes_id` int(11) NOT NULL DEFAULT '0',
+  `sex` int(11) NOT NULL DEFAULT '0',
+  `tel` varchar(20) DEFAULT NULL,
+  `time` date DEFAULT NULL,
+  `remark` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "student"
+#
+
+
+#
+# Structure for table "teacher"
+#
+
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher_no` varchar(20) NOT NULL DEFAULT '',
+  `teacher_name` varchar(20) NOT NULL DEFAULT '',
+  `teacher_pwd` varchar(20) NOT NULL DEFAULT '',
+  `sex` int(11) NOT NULL DEFAULT '0',
+  `tel` varchar(20) DEFAULT NULL,
+  `dep_id` int(11) NOT NULL DEFAULT '0',
+  `time` date DEFAULT NULL,
+  `remark` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "teacher"
+#
+
+
+#
+# Structure for table "teacher_course"
+#
+
+DROP TABLE IF EXISTS `teacher_course`;
+CREATE TABLE `teacher_course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher_id` int(11) NOT NULL DEFAULT '0',
+  `classes_id` int(11) NOT NULL DEFAULT '0',
+  `course_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "teacher_course"
+#
+
+
+#
+# Structure for table "term"
+#
+
+DROP TABLE IF EXISTS `term`;
+CREATE TABLE `term` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `term_name` varchar(20) NOT NULL DEFAULT '',
+  `is_open` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "term"
+#
+
+
+#
+# Structure for table "title"
+#
+
+DROP TABLE IF EXISTS `title`;
+CREATE TABLE `title` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title_con` varchar(50) NOT NULL DEFAULT '',
+  `type` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "title"
+#
+
+
+#
+# Structure for table "tt_score"
+#
+
+DROP TABLE IF EXISTS `tt_score`;
+CREATE TABLE `tt_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher1_id` int(11) NOT NULL DEFAULT '0',
+  `teacher2_id` int(11) NOT NULL DEFAULT '0',
+  `dep_id` int(11) NOT NULL DEFAULT '0',
+  `tt_score` float(5,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "tt_score"
+#
+
